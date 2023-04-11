6274eb866a96196c56e10b00ee391a20dacbeda4
Luiz Henrique <7henrique18@gmail.com>
2023-04-11 20:31:49 -0300

    feat: Check-in use case

---
+++ prisma/migrations/20230411232351_change_user_id_to_user_id/migration.sql
@@ -0,0 +1,15 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `userId` on the `check_ins` table. All the data in the column will be lost.
+
+*/
+-- DropForeignKey
+ALTER TABLE "check_ins" DROP CONSTRAINT "check_ins_userId_fkey";
+
+-- AlterTable
+ALTER TABLE "check_ins" DROP COLUMN "userId",
+ADD COLUMN     "user_id" TEXT;
+
+-- AddForeignKey
+ALTER TABLE "check_ins" ADD CONSTRAINT "check_ins_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE SET NULL ON UPDATE CASCADE;
--- src/use-cases/register/index.spec.ts
+++ src/use-cases/register/index.spec.ts
@@ -50,7 +50,7 @@ describe('Register Use Case', () => {
       password: '123456',
     })
 
-    expect(() =>
+    await expect(() =>
       sut.execute({
         name: 'John Doe',
         email,
