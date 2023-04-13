baf996155905af70c5fb01373be1dafd5c574eb0
Luiz Henrique <7henrique18@gmail.com>
2023-04-13 20:26:12 -0300

    feat: Token update strategy

--- src/app.ts
+++ src/app.ts
@@ -3,17 +3,30 @@ import fastifyJwt from '@fastify/jwt'
 
 import { ZodError } from 'zod'
 import { env } from '@/env'
+import fastifyCookie from '@fastify/cookie'
+
 import { usersRoutes } from './http/controllers/users/routes'
 import { gymsRoutes } from './http/controllers/gyms/routes'
+import { checkInsRoutes } from './http/controllers/check-ins/routes'
 
 export const app = fastify()
 
 app.register(fastifyJwt, {
   secret: env.JWT_SECRET,
+  cookie: {
+    cookieName: 'refreshToken',
+    signed: false,
+  },
+  sign: {
+    expiresIn: '10m',
+  },
 })
+
+app.register(fastifyCookie)
 
 app.register(usersRoutes)
 app.register(gymsRoutes)
+app.register(checkInsRoutes)
 
 app.setErrorHandler((error, _, reply) => {
   if (error instanceof ZodError) {
